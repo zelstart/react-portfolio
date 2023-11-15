09f915c7cb3894cef70377ed73519df0ebfbf558
zelstart <e.pomeroy95@gmail.com>
2023-11-15 15:38:38 -0500

    header and footer set up

---
+++ react-portfolio/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container-fluid">
+                <h4>©2023 zel start. all rights reserved. </h4>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ react-portfolio/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container-fluid d-flex justify-content-center align-items-center">
+        <h1>Zel Start</h1>
+        <nav>
+          <a href="/">about me</a>
+          <a href="/portfolio">portfolio</a>
+          <a href="#">resume</a>
+          <a href="/contactme">contact me</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
