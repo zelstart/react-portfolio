8e4e28819e0113e4740024894521fd5f5e689e98
zelstart <e.pomeroy95@gmail.com>
2023-11-16 17:39:44 -0500

    refactored the about me page

--- react-portfolio/src/components/pages/AboutMe.jsx
+++ react-portfolio/src/components/pages/AboutMe.jsx
@@ -1,51 +1,46 @@
 import React from 'react';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import profilePic from '../../assets/images/profile-pic.jpg';
 import catPeek from '../../assets/images/cat-peek-1.png';
-
-
+{/* <img src={catPeek} alt="cat peeking" className="cat-peek" /> */ }
 
 function AboutMe() {
     return (
-        <main className="container-fluid vh-100 vw-100 d-flex align-items-center mt-3 mt-md-0">
-            <div className="row w-100 justify-content-center">
-                <div className="col-12 col-lg-10">
-                    <Card className='about-me'>
-                        <img src={catPeek} alt="cat peeking" className="cat-peek" />
-                        <Row className='folder'>
-                            <Col xs={6} sm={4} md={3} lg={3}>
-                                <Card.Header className='cstm-card-header'>
-                                    who i am!
-                                </Card.Header>
-                            </Col>
-                            <Col xs={12}>
-                                <Card.Body className='cstm-card-body'>
-                                    <Row className="flex-column flex-md-row px-4">
-                                        <Col xs={12} md={3} lg={2} className="d-flex justify-content-center">
-                                            <img src={profilePic} alt="a person. they look like they hate being photographed!" className="profile-pic" />
-                                        </Col>
-                                        <Col xs={12} md={8} lg={10}  >
-                                            <div className='text-field'>
-                                            <p className='about-me-text'>
-                                                Hello! My name is Zel. I am just entering the world of web development. I am currently on track to graduate the Full Stack Web Development Bootcamp from UNH in mid december! Here's a bunch of text blah blah blah. Gotta write more stuff. Wow. Look at all these things. Could I have used lorem ipsum here? Yeah. Did I feel like finding the generator? No. So here is some stuff.
-                                            </p>
-                                            <p className='about-me-text'>
-                                                Stream of conciousness right onto the page. I wonder how many times a year I think about the Clone Wars. It's gotta be at least two or three times a week. Is this my Roman Empire? It's just. The characters. They're all so lovable. Plo Koon? Plo's Bro's? So wholesome. Fuck the emperor that guy was such a jerk, RIP to my homie Plo Koon.
-                                            </p>
-                                            <p className="about-me-text">
-                                                Wow! Still need to say more? Okay... Well. I have three cats and by gosh by golly they are just the cutest. They're my lil babies. I love them. My oldest is named Riku and he's only got three legs! But he's such a lil trooper. Andi is second oldest and she is an absolute princess. The most well behaved cat I've ever met. Then there's Nyx. He's the youngest, anxious-est, and sweetest! I have truly never met a cat so sweet.
-                                            </p>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                </Card.Body>
-                            </Col>
+        
+        <Row className="about-me vw-100 justify-content-center my-3">
 
-                        </Row>
-                    </Card>
-                </div>
-            </div>
-        </main>
+            <Col xs={4} sm={4} md={4} lg={2} className='cstm-tab'>
+                who i am!
+            </Col>
+            <Col xs={7} sm={7} md={6} lg={8} className='test'></Col>
+
+            
+            <Col xs={11} sm={11} md={10} lg={10} className='cstm-body'>
+                    <Row>
+
+                        <Col xs={12} sm={12} md={3} lg={3} className="d-flex justify-content-center my-1">
+                            <img src={profilePic} alt="a person. they look like they hate being photographed and kinda wish this part was optional!" className="profile-pic" />
+                        </Col>
+
+                        <Col xs={12} sm={12} md={9} lg={9}>
+                            <div className='text-field'>
+                                <p className='about-me-text'>
+                                    Hello! My name is Zel. I am just entering the world of web development. I am currently on track to graduate the Full Stack Web Development Bootcamp from UNH in mid december! Here's a bunch of text blah blah blah. Gotta write more stuff. Wow. Look at all these things. Could I have used lorem ipsum here? Yeah. Did I feel like finding the generator? No. So here is some stuff.
+                                </p>
+                                <p className='about-me-text'>
+                                    Stream of conciousness right onto the page. I wonder how many times a year I think about the Clone Wars. It's gotta be at least two or three times a week. Is this my Roman Empire? It's just. The characters. They're all so lovable. Plo Koon? Plo's Bro's? So wholesome. Fuck the emperor that guy was such a jerk, RIP to my homie Plo Koon.
+                                </p>
+                                <p className="about-me-text">
+                                    Wow! Still need to say more? Okay... Well. I have three cats and they're my lil babies. I love them. My oldest is named Riku and he's only got three legs! But he's such a lil trooper. Andi is second oldest and she is an absolute princess. The most well behaved cat I've ever met. Then there's Nyx. He's the youngest, anxious-est, and sweetest! He's very gentle and loving.
+                                </p>
+                            </div>
+                        </Col>
+
+                    </Row>
+            </Col>
+
+        </Row>
+
     );
 }
 
