8d2c7c24fdf9a6c0f575932ad527d802fce499dd
zelstart <e.pomeroy95@gmail.com>
2023-11-15 21:11:01 -0500

    moved style sheet import to App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import AboutMe from './components/pages/AboutMe';
 import Portfolio from './components/pages/Portfolio';
 import ContactMe from './components/pages/ContactMe';
-
+import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<AboutMe />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<ContactMe />} />
+          <Route path="/contactme" element={<ContactMe />} />
           {/* <Route path="/resume" element={<Resume />} /> */}
         </Routes>
         <Footer />
