62949375e705f5d38735aa56bee0eb68d6ca6e10
zelstart <e.pomeroy95@gmail.com>
2023-11-16 22:05:29 -0500

    added some nifty transitions

--- react-portfolio/src/components/ProjectCard.jsx
+++ react-portfolio/src/components/ProjectCard.jsx
@@ -5,13 +5,13 @@ function ProjectCard({ project }) {
     return (
         <Card className='project-card'>
             <Card.Img variant="top" src={project.image} alt={project.name} className='project-img' />
-            <div className="card-img-overlay d-flex flex-column justify-content-end">
+            <div className="card-img-overlay d-flex flex-column justify-content-end project-details">
                 <Card.Title>{project.name}</Card.Title>
-                <Card.Text>
+                <Card.Text className="project-description">
                     {project.description}
                 </Card.Text>
-                <a href={project.githubRepo} target="_blank" rel="noopener noreferrer">github repo &gt;</a>
-                {project.liveWebsite && <a href={project.liveWebsite} target="_blank" rel="noopener noreferrer">live site &gt;</a>}
+                <a href={project.githubRepo} target="_blank" rel="noopener noreferrer" className="project-link">github repo &gt;</a>
+                {project.liveWebsite && <a href={project.liveWebsite} target="_blank" rel="noopener noreferrer" className="project-link">live site &gt;</a>}
             </div>
         </Card>
     )
