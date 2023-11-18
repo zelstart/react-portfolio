b9e76b69ddb35c5548200facb5d28bf73f776cee
zelstart <e.pomeroy95@gmail.com>
2023-11-17 21:53:50 -0500

    split the header and nav into 2 comps

--- react-portfolio/src/components/Header/Header.jsx
+++ react-portfolio/src/components/Header/Header.jsx
@@ -4,20 +4,8 @@ import '../../style.css';
 
 function Header() {
   return (
-    <header className="header">
-      <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
-        <div>
-          <h1>Zel Start</h1>
-        </div>
-        <div>
-          <nav>
-            <a href="/">about me</a>
-            <a href="/portfolio">portfolio</a>
-            <a href="#">resume</a>
-            <a href="/contactme">contact me</a>
-          </nav>
-        </div>
-      </div>
+    <header className="header container-fluid d-flex flex-column justify-content-center align-items-center">
+          <h1>hello!</h1>
     </header>
   );
 }
---
+++ react-portfolio/src/components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Nav.css';
+import '../../style.css';
+
+function Nav() {
+    return (
+        <nav className="container-fluid d-flex flex-column justify-content-center align-items-center">
+                <div>
+                    <a className='navlink' href="/">about me</a>
+                    <a className='navlink' href="/portfolio">portfolio</a>
+                    <a className='navlink' href="#">resume</a>
+                    <a className='navlink' href="/contactme">contact me</a>
+                </div>
+        </nav>
+    )
+
+
+}
+
+export default Nav;
