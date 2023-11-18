c71731dc839b039afc126927d6241a1703659387
zelstart <e.pomeroy95@gmail.com>
2023-11-18 01:17:18 -0500

    trying to implement a dark mode

--- react-portfolio/src/components/Header/Header.jsx
+++ react-portfolio/src/components/Header/Header.jsx
@@ -1,18 +1,36 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Header.css';
 import '../../style.css';
 
-function Header() {
-  const h1Ref = useRef();
+// check darkMode value in localStorage and set class on body before component renders
+// trying to stop annoying flash of white between page loads
+if (localStorage.getItem('darkMode') === 'true') {
+  document.body.classList.add('dark-mode');
+}
 
+function Header() {
+  // dark mode
+  const h1Ref = useRef();
+  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') === 'true');
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
+  // set dark mode class on body and save darkMode value to localStorage
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
+  // type effect
   useEffect(() => {
     const hasTyped = localStorage.getItem('hasTyped');
 
-    if (!hasTyped) {
+    if (!hasTyped) { 
       const typeEffect = (element, speed) => {
         let text = element.innerHTML;
         element.innerHTML = "";
-
         let i = 0;
         let timer = setInterval(function() {
           if (i < text.length) {
@@ -24,19 +42,22 @@ function Header() {
         }, speed);
       }
 
-      typeEffect(h1Ref.current, 150); // runs the effect on page load
+      typeEffect(h1Ref.current, 150); 
 
-      // setTimeout(() => {
-      //   h1Ref.current.classList.add('hide-cursor');
-      // }, 20000); // hides the cursor after 20 seconds
+      /*
+      setTimeout(() => {
+        h1Ref.current.classList.add('hide-cursor');
+      }, 20000); // hidse the cursor after 20 seconds
+      */
 
       localStorage.setItem('hasTyped', 'true');
     }
-  }, []); // stops the effect from running on every render
+  }, []);
 
   return (
     <header className="header container-fluid d-flex flex-column justify-content-center align-items-center">
       <h1 ref={h1Ref}>hello!</h1>
+      <button className="dark-mode-toggle" onClick={toggleDarkMode}>Toggle Dark Mode</button>
     </header>
   );
 }
