424996ba9a2dd1617f6253788af75c694bc22440
zelstart <e.pomeroy95@gmail.com>
2023-11-18 18:39:41 -0500

    overhauled the about me page

--- react-portfolio/src/components/pages/AboutMe/AboutMe.jsx
+++ react-portfolio/src/components/pages/AboutMe/AboutMe.jsx
@@ -1,49 +1,33 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Card, Row, Col, Container, Tabs, Tab } from 'react-bootstrap';
 import './AboutMe.css';
 import '../../../style.css';
 import profilePic from '../../../assets/images/profile-pic.jpg';
-import catPeek from '../../../assets/images/cat-peek-1.png';
+import Skills from '../../Skills/Skills'; 
 
-{/* <img src={catPeek} alt="cat peeking" className="cat-peek" /> */ }
 
 function AboutMe() {
     return (
-
-        <Row className="about-me justify-content-center my-3">
-
-            <Col xs={4} sm={4} md={3} lg={2} className='cstm-tab'>
-                who i am!
+        <Container fluid>
+        <Row className="justify-content-center overflow-x-hidden">
+            <Col className="image-container" lg={2}>
+                <img src={profilePic} alt="Profile" className="profile-pic drop-image" />
             </Col>
-            <Col xs={7} sm={7} md={7} lg={8} className='test'></Col>
-
-
-            <Col xs={11} sm={11} md={10} lg={10} className='cstm-body'>
-                <Row>
-
-                    <Col xs={12} sm={12} md={4} lg={3} className="d-flex justify-content-center my-1 order-1 order-md-0">
-                        <img src={profilePic} alt="a person. they look like they hate being photographed and kinda wish this part was optional!" className="profile-pic" />
-                    </Col>
-
-                    <Col xs={12} sm={12} md={8} lg={9} className="order-0 order-md-1">
-                        <div className='text-field'>
-                            <p className='about-me-text'>
-                                Hello! My name is Zel. I am just entering the world of web development. I am currently on track to graduate the Full Stack Web Development Bootcamp from UNH in mid december! Here's a bunch of text blah blah blah. Gotta write more stuff. Wow. Look at all these things. Could I have used lorem ipsum here? Yeah. Did I feel like finding the generator? No. So here is some stuff.
-                            </p>
-                            <p className='about-me-text'>
-                                Stream of conciousness right onto the page. I wonder how many times a year I think about the Clone Wars. It's gotta be at least two or three times a week. Is this my Roman Empire? It's just. The characters. They're all so lovable. Plo Koon? Plo's Bro's? So wholesome. Fuck the emperor that guy was such a jerk, RIP to my homie Plo Koon.
-                            </p>
-                            <p className="about-me-text">
-                                Wow! Still need to say more? Okay... Well. I have three cats and they're my lil babies. I love them. My oldest is named Riku and he's only got three legs! But he's such a lil trooper. Andi is second oldest and she is an absolute princess. The most well behaved cat I've ever met. Then there's Nyx. He's the youngest, anxious-est, and sweetest! He's very gentle and loving.
-                            </p>
-                        </div>
-                    </Col>
-
-                </Row>
+            <Col className='' lg={7}>
+            <Tabs defaultActiveKey="who i am" id="">
+                <Tab eventKey="who-i-am" title="who i am">
+                    <p className='about-me-text'>Hey! My name is Zel, and I'm fairly new to the web development scene. I am on track to graduate from UNH's Full Stack Web Development bootcamp in December. I have always liked tinkering with HTML and CSS on sites like MySpace, Tumblr and Proboards, but in May of 2023, I decided I wanted to completely change my career path and have spent the last few months learning a lot of different skills to help me break into this field. </p>
+                </Tab>
+                <Tab className='' eventKey="skills" title="what i can do">
+                    <Skills /> 
+                </Tab>
+                <Tab className='' eventKey="goals" title="what i want to do">
+                    <p className='about-me-text'> </p>
+                </Tab>
+            </Tabs>
             </Col>
-
         </Row>
-
+        </Container>
     );
 }
 
