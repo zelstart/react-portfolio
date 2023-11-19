fecc4f468e78b8397a0d77777c231c148e65ba92
zelstart <e.pomeroy95@gmail.com>
2023-11-19 00:28:21 -0500

    reorganized for build

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
