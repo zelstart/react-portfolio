a1a03193fe8d1e3a77dd97a3f1f0dd7ae2a35321
zelstart <e.pomeroy95@gmail.com>
2023-11-19 02:05:31 -0500

    imported images instead

--- react-portfolio/src/assets/data/data.js
+++ react-portfolio/src/assets/data/data.js
@@ -1,3 +1,9 @@
+import SocialNetWorkAPI from '../images/project-images/placeholder-image.jpg';
+import TechBlog from '../images/project-images/the-tech-blog.png';
+import ECommerceBackend from '../images/project-images/pexels-544917.jpg';
+import WeatherDashboard from '../images/project-images/weather-dashboard.png';
+import Tunecast from '../images/project-images/tunecast.png';
+import MyRecipeBook from '../images/project-images/my-recipe-book.png';
 
 
 const projectData = [
@@ -6,42 +12,42 @@ const projectData = [
         description: 'A RESTful API for a social network web application where users can share their thoughts, react to friends’ thoughts, and add other users to their friends list.',
         liveWebsite: null,
         githubRepo: 'https://github.com/zelstart/social-network-api',
-        image: '/project-images/placeholder-image.jpg',
+        image: SocialNetWorkAPI,
     },
     {
         name: 'Tech Blog',
         description: 'A CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well.',
         liveWebsite: 'https://zelstart-tech-blog-7449797524ee.herokuapp.com/',
         githubRepo: 'https://github.com/zelstart/social-network-api',
-        image: '/project-images/the-tech-blog.png',
+        image: TechBlog,
     },
     {
         name: 'e-commerce backend',
         description: 'This E-Commerce Back End is a straightforward backend solution designed for managing categories, products, and tags in an e-commerce system.',
         liveWebsite: null,
         githubRepo: 'https://github.com/zelstart/e-commerce-back-end',
-        image: '/project-images/pexels-544917.jpg',
+        image: ECommerceBackend,
     },
     {
         name: 'Weather Dashboard',
         description: 'A simple weather dashboard that allows the user to search for a US city and view the current weather conditions as well as the 5-day forecast for that city.',
         liveWebsite: null,
         githubRepo: 'https://github.com/zelstart/weather-dashboard',
-        image: '/project-images/weather-dashboard.png',
+        image: WeatherDashboard,
     },
     {
         name: 'Tunecast // Group Project',
         description: 'A web application using the Weather API and the YouTube API to fetch the users weather forecast and provide them with a playlist of songs based on their weather conditions.',
         liveWebsite: 'https://zelstart.github.io/tunecast/',
         githubRepo: 'https://github.com/zelstart/tunecast',
-        image: '/project-images/tunecast.png',
+        image: Tunecast,
     },
     {
         name: 'My Recipe Book // Group Project',
         description: 'A web application that allows users to keep their recipes, and even discover new ones submitted by other users.',
         liveWebsite: 'https://myrecipebook-ztc-5e1e57d00e6c.herokuapp.com/',
         githubRepo: 'https://github.com/zelstart/my-recipe-book',
-        image: '/project-images/my-recipe-book.png',
+        image: MyRecipeBook,
     },
 ];
 
