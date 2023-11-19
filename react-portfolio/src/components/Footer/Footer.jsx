32be4fad2020d39a05b91d37dfe8201487c22abc
zelstart <e.pomeroy95@gmail.com>
2023-11-18 22:10:46 -0500

    added social media icons

--- react-portfolio/src/components/Footer/Footer.jsx
+++ react-portfolio/src/components/Footer/Footer.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './Footer.css';
 import '../../style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Footer() {
     return (
         <footer className="footer">
-            <div className="container-fluid">
-                <h4>©2023 zel start. all rights reserved. </h4>
-            </div>
+            <Container fluid>
+                <Row className="justify-content-center">
+                    <Col xs="auto">
+                        <a href='https://github.com/zelstart' target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className='my-icons' /></a>
+                        <a href='https://www.linkedin.com/in/zel-start-8b05382a0/' target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className='my-icons' /></a>
+                        <a href='https://stackoverflow.com/users/19686794/zel' target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faStackOverflow} className='my-icons' /></a>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col xs="auto">
+                        <h4>©2023 zel start. all rights reserved.</h4>
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     )
 }
