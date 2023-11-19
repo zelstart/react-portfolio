24e54fbdcf0322e6283ce50fd8bfc4315130029e
zelstart <e.pomeroy95@gmail.com>
2023-11-19 02:50:10 -0500

    added goals to the about me page

--- react-portfolio/src/components/pages/AboutMe/AboutMe.jsx
+++ react-portfolio/src/components/pages/AboutMe/AboutMe.jsx
@@ -3,7 +3,8 @@ import { Card, Row, Col, Container, Tabs, Tab } from 'react-bootstrap';
 import './AboutMe.css';
 import '../../../style.css';
 import profilePic from '../../../assets/images/profile-pic.jpg';
-import Skills from '../../Skills/Skills'; 
+import Skills from '../../Skills/Skills';
+import Goals from '../../Goals/Goals';
 
 
 function AboutMe() {
@@ -15,18 +16,22 @@ function AboutMe() {
                 </Col>
                 <Col className='order-1 order-md-2' lg={7} md={7} sm={12}>
                     <Tabs defaultActiveKey="who-i-am" id="">
-                <Tab eventKey="who-i-am" title="who i am">
-                    <p className='about-me-text'>Hey! My name is Zel, and I'm fairly new to the web development scene. I am on track to graduate from UNH's Full Stack Web Development bootcamp in December. I have always liked tinkering with HTML and CSS on sites like MySpace, Tumblr and Proboards, but in May of 2023, I decided I wanted to completely change my career path and have spent the last few months learning a lot of different skills to help me break into this field. </p>
-                </Tab>
-                <Tab className='' eventKey="skills" title="what i can do">
-                    <Skills /> 
-                </Tab>
-                <Tab className='' eventKey="goals" title="what i want to do">
-                    <p className='about-me-text'> </p>
-                </Tab>
-            </Tabs>
-            </Col>
-        </Row>
+
+                        <Tab eventKey="who-i-am" title="who i am">
+                            <p className='about-me-text'>Hey! My name is Zel, and I'm fairly new to the web development scene. I am on track to graduate from UNH's Full Stack Web Development bootcamp in December. I have always liked tinkering with HTML and CSS on sites like MySpace, Tumblr and Proboards, but in May of 2023, I decided I wanted to completely change my career path and have spent the last few months learning a lot of different skills to help me break into this field. </p>
+                        </Tab>
+
+                        <Tab className='' eventKey="skills" title="what i can do">
+                            <Skills />
+                        </Tab>
+
+                        <Tab className='' eventKey="goals" title="what i want to do">
+                            <Goals />
+                        </Tab>
+
+                    </Tabs>
+                </Col>
+            </Row>
         </Container>
     );
 }
