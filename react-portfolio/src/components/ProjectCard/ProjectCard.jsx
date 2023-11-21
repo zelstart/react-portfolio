8e04a9abad5e00cab93378f23ec40990c2298c7f
zelstart <e.pomeroy95@gmail.com>
2023-11-20 20:14:59 -0500

    fixed accidentally opening links on mobile

--- react-portfolio/src/components/ProjectCard/ProjectCard.jsx
+++ react-portfolio/src/components/ProjectCard/ProjectCard.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 
-
 function ProjectCard({ project }) {
-    const [isHovered, setIsHovered] = useState(false);
+    const [isTapped, setIsTapped] = useState(false);
+
+    const handleCardTap = (e) => {
+        if (!isTapped) {
+            e.preventDefault();
+            setIsTapped(true);
+        }
+    }
 
     return (
-        <Card className='project-card' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-            <Card.Img variant="top" src={project.image} alt={project.name} className={`project-img ${isHovered ? 'darken' : ''}`} />
+        <Card className='project-card' onClick={handleCardTap}>
+            <Card.Img variant="top" src={project.image} alt={project.name} className={`project-img ${isTapped ? 'darken' : ''}`} />
             <div className="card-img-overlay d-flex flex-column justify-content-end">
                 <Card.Title className='project-title'>{project.name}</Card.Title>
-                <div className="project-description-container">
-                    <Card.Text className="project-description">
-                        {project.description}
-                    </Card.Text>
-                    <a href={project.githubRepo} target="_blank" rel="noopener noreferrer" className="project-link">github repo &gt;</a>
-                    {project.liveWebsite && <a href={project.liveWebsite} target="_blank" rel="noopener noreferrer" className="project-link">live site &gt;</a>}
-                </div>
+                {isTapped && (
+                    <div className="project-description-container">
+                        <Card.Text className="project-description">
+                            {project.description}
+                        </Card.Text>
+                        <a href={project.githubRepo} target="_blank" rel="noopener noreferrer" className="project-link">github repo &gt;</a>
+                        {project.liveWebsite && <a href={project.liveWebsite} target="_blank" rel="noopener noreferrer" className="project-link">live site &gt;</a>}
+                    </div>
+                )}
             </div>
         </Card>
     )
