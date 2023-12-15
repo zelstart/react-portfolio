59524fdd822e56f6aa7cce31cbee4864522ac064
Zel Start <134749644+zelstart@users.noreply.github.com>
2023-12-15 16:33:06 -0500

    added new project

--- react-portfolio/src/assets/data/data.js
+++ react-portfolio/src/assets/data/data.js
@@ -28,13 +28,13 @@ const projectData = [
         githubRepo: 'https://github.com/zelstart/e-commerce-back-end',
         image: ECommerceBackend,
     },
-    {
-        name: 'Weather Dashboard',
-        description: 'A simple weather dashboard that allows the user to search for a US city and view the current weather conditions as well as the 5-day forecast for that city.',
-        liveWebsite: 'https://zelstart.github.io/weather-dashboard',
-        githubRepo: 'https://github.com/zelstart/weather-dashboard',
-        image: WeatherDashboard,
-    },
+    // {
+    //     name: 'Weather Dashboard',
+    //     description: 'A simple weather dashboard that allows the user to search for a US city and view the current weather conditions as well as the 5-day forecast for that city.',
+    //     liveWebsite: 'https://zelstart.github.io/weather-dashboard',
+    //     githubRepo: 'https://github.com/zelstart/weather-dashboard',
+    //     image: WeatherDashboard,
+    // },
     {
         name: 'Tunecast // Group Project',
         description: 'A web application using the Weather API and the YouTube API to fetch the users weather forecast and provide them with a playlist of songs based on their weather conditions.',
@@ -49,6 +49,13 @@ const projectData = [
         githubRepo: 'https://github.com/zelstart/my-recipe-book',
         image: MyRecipeBook,
     },
+    {
+        name: 'Pokémon Team Builder // Group Project',
+        description: 'A web application that allows users to build their own Pokémon team.',
+        liveWebsite: 'https://pokemon-team-builder-zs.netlify.app/',
+        githubRepo: 'https://github.com/zelstart/pokemon-team-builder-clone',
+        image: PokemonTeamBuilder,
+    }
 ];
 
 // TODO: Add another property to each object called "proficiency" and set it to a number between 1 and 3
